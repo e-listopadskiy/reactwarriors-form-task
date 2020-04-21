9057e602cff23b4def42d2c8bb043676c6295f4b
unknown <e.listopadskiy@gmail.com>
2020-04-21 03:19:18 +0300

    Add

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="form-container card">
-        <form className="form card-body" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="form-container card">
+				<form className="form card-body" />
+				)_
+			</div>
+		);
+	}
 }
